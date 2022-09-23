7c97bbc881cd5b5b6997b0f3d091e1ad458f88c0
mizankamal7 <mizan@sicepat.com>
2022-09-23 16:37:11 +0700

    my course web

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function clickHome () {}
+            alert("Ini Halaman Home");
+        function clickCourse () {}
+            alert("Ini Halaman Home");
+        function clickBlog () {}
+            alert("Ini Halaman Home");
